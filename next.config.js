67bea2ecb01c17392ad84de27f4ac370183d7857
Curt Siffert <curt@keenworks.com>
2022-03-27 16:09:26 -0700

    Change lang approach for next export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   assetPrefix: !debug ? "/adnd-combat-tools/" : "",
   basePath: !debug ? "/adnd-combat-tools" : "",
 };
