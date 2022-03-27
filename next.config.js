8b3d47ec4c737e670b20f9fa426edf83ff3226a2
Curt Siffert <curt@keenworks.com>
2022-03-27 15:08:56 -0700

    Set lang so it doesn't think it's Romanian

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   assetPrefix: !debug ? "/adnd-combat-tools/" : "",
   basePath: !debug ? "/adnd-combat-tools" : "",
 };
