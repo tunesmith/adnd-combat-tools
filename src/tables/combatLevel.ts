72b39b5d19c88fa7f181d32662870c6fca41a08e
Curt Siffert <curt@keenworks.com>
2022-04-17 13:49:03 -0700

    Simplify types for combatLevel

--- src/tables/combatLevel.ts
+++ src/tables/combatLevel.ts
@@ -1,13 +1,8 @@
 import { LevelOption } from "../types/option";
 import { BARD, CLERIC, FIGHTER, MAGIC_USER, THIEF } from "./attackerClass";
 
-interface MonsterLevelProps {
-  label: string;
-  thaco: number;
-}
-interface ClassLevelProps {
-  level: number;
-  label: string;
+interface LevelProps {
+  label?: string;
   thaco: number;
 }
 
@@ -16,15 +11,9 @@ interface ClassLevelProps {
  * Therefore, "pick the class most beneficial to combat in terms of thaco and armor type"
  * Then for level, "pick the level of the class"
  *
- * But then I screw it up for bard. I like bard for restrictive armor,
- * but it's really the level for fighter. Yuck.
- *
  * A bard always engages in combat at the level he or she attained as a fighter.
- *
- * This is easy after all - I can make the level labels be "Fighter L1", etc,
- * and there are only eight options.
  */
-export const monsterLevels = new Map<number, MonsterLevelProps>([
+export const monsterLevels = new Map<number, LevelProps>([
   [1, { label: "up to 1-1", thaco: 21 }],
   [2, { label: "1-1", thaco: 20 }],
   [3, { label: "1", thaco: 19 }],
@@ -38,48 +27,48 @@ export const monsterLevels = new Map<number, MonsterLevelProps>([
   [11, { label: "14-15+", thaco: 8 }],
   [12, { label: "16+", thaco: 7 }],
 ]);
-const fighterLevels = new Map<number, ClassLevelProps>([
-  [0, { level: 0, label: "0", thaco: 21 }],
-  [1, { level: 1, label: "1", thaco: 20 }],
-  [3, { level: 3, label: "3", thaco: 18 }],
-  [5, { level: 5, label: "5", thaco: 16 }],
-  [7, { level: 7, label: "7", thaco: 14 }],
-  [9, { level: 9, label: "9", thaco: 12 }],
-  [11, { level: 11, label: "11", thaco: 10 }],
-  [13, { level: 13, label: "13", thaco: 8 }],
-  [15, { level: 15, label: "15", thaco: 6 }],
-  [17, { level: 17, label: "17+", thaco: 4 }],
+const fighterLevels = new Map<number, LevelProps>([
+  [0, { thaco: 21 }],
+  [1, { thaco: 20 }],
+  [3, { thaco: 18 }],
+  [5, { thaco: 16 }],
+  [7, { thaco: 14 }],
+  [9, { thaco: 12 }],
+  [11, { thaco: 10 }],
+  [13, { thaco: 8 }],
+  [15, { thaco: 6 }],
+  [17, { thaco: 4 }],
 ]);
-const clericLevels = new Map<number, ClassLevelProps>([
-  [1, { level: 1, label: "1", thaco: 20 }],
-  [4, { level: 4, label: "4", thaco: 18 }],
-  [7, { level: 7, label: "7", thaco: 16 }],
-  [10, { level: 10, label: "10", thaco: 14 }],
-  [13, { level: 13, label: "13", thaco: 12 }],
-  [16, { level: 16, label: "16", thaco: 10 }],
-  [19, { level: 19, label: "19+", thaco: 9 }],
+const clericLevels = new Map<number, LevelProps>([
+  [1, { thaco: 20 }],
+  [4, { thaco: 18 }],
+  [7, { thaco: 16 }],
+  [10, { thaco: 14 }],
+  [13, { thaco: 12 }],
+  [16, { thaco: 10 }],
+  [19, { thaco: 9 }],
 ]);
-const thiefLevels = new Map<number, ClassLevelProps>([
-  [1, { level: 1, label: "1", thaco: 21 }],
-  [5, { level: 5, label: "5", thaco: 19 }],
-  [9, { level: 9, label: "9", thaco: 16 }],
-  [13, { level: 13, label: "13", thaco: 14 }],
-  [17, { level: 17, label: "17", thaco: 12 }],
-  [21, { level: 21, label: "21+", thaco: 10 }],
+const thiefLevels = new Map<number, LevelProps>([
+  [1, { thaco: 21 }],
+  [5, { thaco: 19 }],
+  [9, { thaco: 16 }],
+  [13, { thaco: 14 }],
+  [17, { thaco: 12 }],
+  [21, { thaco: 10 }],
 ]);
-const magicUserLevels = new Map<number, ClassLevelProps>([
-  [1, { level: 1, label: "1", thaco: 21 }],
-  [6, { level: 6, label: "6", thaco: 19 }],
-  [11, { level: 11, label: "11", thaco: 16 }],
-  [16, { level: 16, label: "16", thaco: 13 }],
-  [21, { level: 21, label: "21+", thaco: 11 }],
+const magicUserLevels = new Map<number, LevelProps>([
+  [1, { thaco: 21 }],
+  [6, { thaco: 19 }],
+  [11, { thaco: 16 }],
+  [16, { thaco: 13 }],
+  [21, { thaco: 11 }],
 ]);
-const bardLevels = new Map<number, ClassLevelProps>([
-  [1, { level: 1, label: "1", thaco: 20 }],
-  [3, { level: 3, label: "3", thaco: 18 }],
-  [5, { level: 5, label: "5", thaco: 16 }],
-  [7, { level: 7, label: "7", thaco: 14 }],
-  [8, { level: 8, label: "8+", thaco: 14 }],
+const bardLevels = new Map<number, LevelProps>([
+  [1, { thaco: 20 }],
+  [3, { thaco: 18 }],
+  [5, { thaco: 16 }],
+  [7, { thaco: 14 }],
+  [8, { thaco: 14 }],
 ]);
 
 /**
@@ -88,7 +77,7 @@ const bardLevels = new Map<number, ClassLevelProps>([
  * @param targetLevel
  */
 const getLevelThaco = (
-  levelProps: Map<number, ClassLevelProps>,
+  levelProps: Map<number, LevelProps>,
   targetLevel: number
 ): number =>
   Array.from(levelProps).reduce<number>(
@@ -147,7 +136,7 @@ export const getThac = (ac: number, thaco: number) => {
 };
 
 const getClassLevels = (
-  levels: Map<number, ClassLevelProps>,
+  levels: Map<number, LevelProps>,
   bard: boolean = false
 ): LevelOption[] => {
   const keys: number[] = Array.from(levels.keys());
