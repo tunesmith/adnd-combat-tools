f446eef4b24a5552a424b98c5f0454ecbd0d6ac3
Curt Siffert <curt@keenworks.com>
2024-11-10 10:58:09 -0800

    dungeon: side passage completed

--- src/dungeon/services/sidePassage.ts
+++ src/dungeon/services/sidePassage.ts
@@ -1,6 +1,11 @@
 import { SidePassages, sidePassages } from "../../tables/dungeon/sidePassages";
 import { getTableEntry, rollDice } from "./passage";
 
+/**
+ * We do *not* check passage width for side passages, as the "periodic check"
+ * table specifically calls out passage width for "passage turns" but not for
+ * side passages.
+ */
 export const sidePassageResults = (): string => {
   const sidePassageRoll = rollDice(sidePassages.sides);
   const sidePassageCommand = getTableEntry(sidePassageRoll, sidePassages);
@@ -10,31 +15,31 @@ export const sidePassageResults = (): string => {
     );
     switch (sidePassageCommand as SidePassages) {
       case SidePassages.Left90:
-        return "A side passage branches left 90 degrees. The main passage extends ahead 30'. ";
+        return "A side passage branches left 90 degrees. Passages extend -- check again in 30'. ";
       case SidePassages.Right90:
-        return "A side passage branches right 90 degrees. The main passage extends ahead 30'. ";
+        return "A side passage branches right 90 degrees. Passages extend -- check again in 30'. ";
       case SidePassages.Left45:
-        return "A side passage branches left 45 degrees ahead. The main passage extends ahead 30'. ";
+        return "A side passage branches left 45 degrees ahead. Passages extend -- check again in 30'. ";
       case SidePassages.Right45:
-        return "A side passage branches right 45 degrees ahead. The main passage extends ahead 30'. ";
+        return "A side passage branches right 45 degrees ahead. Passages extend -- check again in 30'. ";
       case SidePassages.Left135:
-        return "A side passage branches left 45 degrees behind (left 135 degrees). The main passage extends ahead 30'. ";
+        return "A side passage branches left 45 degrees behind (left 135 degrees). Passages extend -- check again in 30'. ";
       case SidePassages.Right135:
-        return "A side passage branches right 45 degrees behind (right 135 degrees). The main passage extends ahead 30'. ";
+        return "A side passage branches right 45 degrees behind (right 135 degrees). Passages extend -- check again in 30'. ";
       case SidePassages.LeftCurve45:
-        return "A side passage branches at a curve, 45 degrees left ahead. The main passage extends ahead 30'. ";
+        return "A side passage branches at a curve, 45 degrees left ahead. Passages extend -- check again in 30'. ";
       case SidePassages.RightCurve45:
-        return "A side passage branches at a curve, 45 degrees right ahead. The main passage extends ahead 30'. ";
+        return "A side passage branches at a curve, 45 degrees right ahead. Passages extend -- check again in 30'. ";
       case SidePassages.PassageT:
-        return "The passage reaches a 'T' intersection to either side, extending 30' both directions. ";
+        return "The passage reaches a 'T' intersection to either side. Passages extend -- check again in 30'. ";
       case SidePassages.PassageY:
-        return "The passage reaches a 'Y' intersection, each extending 30', ahead 45 degrees to the left and right. ";
+        return "The passage reaches a 'Y' intersection, ahead 45 degrees to the left and right. Passages extend -- check again in 30'. ";
       case SidePassages.FourWay:
-        return "The passage reaches a four-way intersection, extending 30' in all directions. ";
+        return "The passage reaches a four-way intersection. Passages extend -- check again in 30'. ";
       case SidePassages.PassageX:
         return (
-          "The passage reaches an 'X' intersection, extending 30' in all directions. (If the present passage " +
-          "is horizontal or vertical, it forms a fifth passage into the 'X'). "
+          "The passage reaches an 'X' intersection. (If the present passage " +
+          "is horizontal or vertical, it forms a fifth passage into the 'X'.) Passages extend -- check again in 30'. "
         );
     }
   } else {
