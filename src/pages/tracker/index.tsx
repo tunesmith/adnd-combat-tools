cc6e6fcb01aca217c8bddcdc838558ef22c68ece
Curt Siffert <curt@keenworks.com>
2026-03-22 16:57:01 -0700

    tracker: improve how it reads from state

--- src/pages/tracker/index.tsx
+++ src/pages/tracker/index.tsx
@@ -3,21 +3,41 @@ import { useEffect, useState } from "react";
 import CombatTracker from "../../components/tracker/CombatTracker";
 import TrackerDecoder from "../../components/tracker/TrackerDecoder";
 
+const getEncodedStateFromLocation = (): string | undefined => {
+  if (typeof window === "undefined") {
+    return undefined;
+  }
+
+  const encodedState = new URLSearchParams(window.location.search).get("s");
+  return encodedState || undefined;
+};
+
 const TrackerIndexPage = () => {
   const router = useRouter();
   const [loaded, setLoaded] = useState<boolean>(false);
-  const { s: state } = router.query;
+  const [encodedState, setEncodedState] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     if (!router.isReady) {
       return;
     }
-    setLoaded(true);
+
+    const syncFromLocation = () => {
+      setEncodedState(getEncodedStateFromLocation());
+      setLoaded(true);
+    };
+
+    syncFromLocation();
+    window.addEventListener("popstate", syncFromLocation);
+
+    return () => {
+      window.removeEventListener("popstate", syncFromLocation);
+    };
   }, [router.isReady]);
 
   return loaded ? (
-    state ? (
-      <TrackerDecoder encodedState={state as string} />
+    encodedState ? (
+      <TrackerDecoder encodedState={encodedState} />
     ) : (
       <CombatTracker />
     )
